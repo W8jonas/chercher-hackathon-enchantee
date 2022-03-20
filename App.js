423c85e067253e42f880aba9ba197cc9703d5082
Jonas <jonashenrique989@gmail.com>
2022-03-20 03:35:43 -0300

    remanejando para routes

--- App.js
+++ App.js
@@ -2,13 +2,14 @@ import 'react-native-gesture-handler';
 
 import { StatusBar } from 'expo-status-bar'
 import { StyleSheet, Text, View } from 'react-native'
-import { Index } from './source'
+import { Routes } from './source/routes';
 
 
 export default function App() {
 	return (
 		<View style={styles.container}>
-			<Index />
+			<StatusBar style="dark" />
+			<Routes />
 		</View>
 	)
 }
