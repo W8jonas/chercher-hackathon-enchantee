6d7fd35f03b75105ab68eb46d6ffc1c3df392552
Jonas <jonashenrique989@gmail.com>
2022-03-20 12:30:41 -0300

    Ajustando Routes

--- source/routes/index.js
+++ source/routes/index.js
@@ -6,11 +6,11 @@ import { createStackNavigator } from '@react-navigation/stack'
 import AppLoading from 'expo-app-loading'
 
 import { SplashScreen } from '../screens/SplashScreen'
-// import { Intro } from '../screens/Intro'
-// import { Tutorial } from '../screens/Tutorial'
+import { IntroScreen } from '../screens/Intro'
+import { TutorialScreen } from '../screens/Tutorial'
 // import { MapCountry } from '../screens/MapCountry'
 import { Game } from '../screens/Game'
-// import { Score } from '../screens/Score'
+import { Score } from '../screens/Score'
 
 import { routesPaths } from './routesPaths'
 
@@ -34,11 +34,11 @@ export function Routes() {
         <NavigationContainer>
             <Stack.Navigator screenOptions={{headerShown: false}}>
                 <Stack.Screen name={routesPaths.splash} component={SplashScreen} />
-                {/* <Stack.Screen name={routesPaths.intro} component={Intro} /> */}
-                {/* <Stack.Screen name={routesPaths.tutorial} component={Tutorial} /> */}
+                <Stack.Screen name={routesPaths.intro} component={IntroScreen} />
+                <Stack.Screen name={routesPaths.tutorial} component={TutorialScreen} />
                 {/* <Stack.Screen name={routesPaths.mapCountry} component={MapCountry} /> */}
                 <Stack.Screen name={routesPaths.game} component={Game} />
-                {/* <Stack.Screen name={routesPaths.score} component={Score} /> */}
+                <Stack.Screen name={routesPaths.score} component={Score} />
             </Stack.Navigator>
         </NavigationContainer>
     )
